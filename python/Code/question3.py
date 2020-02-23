e8e22d49855a6d96457d421ec6e5c9658f21faa8
Douglas <douglas@lapisco.ifce.edu.br>
2020-02-23 14:14:41 -0300

    Question 3

--- python/Code/question3.py
+++ python/Code/question3.py
@@ -1,2 +1,14 @@
 import cv2
 
+img = cv2.imread("../inputimage/q1inputimage.jpeg")
+
+redimage = img[:, :, 2]
+greenimage = img[:, :, 1]
+blueimage =img[:, :, 0]
+
+
+cv2.imwrite("../outputimage/q3redimage.png", redimage)
+cv2.imwrite("../outputimage/q3greenimage.png", greenimage)
+cv2.imwrite("../outputimage/q3blueimage.png", blueimage)
+
+
