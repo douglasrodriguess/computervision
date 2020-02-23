cd13899193572abdbe87ade80b617146ad45a8bc
Douglas <douglas@lapisco.ifce.edu.br>
2020-02-23 15:47:09 -0300

    question 9 and 10

--- python/outputimage/question10.py
+++ python/outputimage/question10.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy
 
 image = cv2.imread("../inputimage/q1inputimage.jpeg")
 grayimage = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
@@ -6,8 +7,13 @@ grayimage = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 sizeX, sizeY = grayimage.shape[:2]
 print(sizeX, sizeY)
 
-with open ("../outputimage/pixelquestion9.txt", "w") as file:
+thresh = 100
+
+with open ("../outputimage/pixelquestion10.txt", "w") as file:
     for rows in range(sizeX):
         for columns in range(sizeY):
-            file.write(str(grayimage[rows, columns]) + ' ')
+            if (grayimage[rows, columns] > thresh):
+                file.write('255' + ' ')
+            else:
+                file.write('0' + ' ')
         file.write("\n")
